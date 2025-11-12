158d65ebbcde8106b9eb69d0c9b29883e61bd314
olehoffmann <ole_niclas_hoffmann@yahoo.com>
2025-11-12 14:15:30 +0100

    Upload before changing to Basalt version

--- include/GPUFrontend.h
+++ include/GPUFrontend.h
@@ -14,20 +14,27 @@ public:
     void stagingBufferImage(cv::Mat image, int width, int height, int pixelbytes);
     void createComputePipeline(const std::string& shaderPath);
     void createDescriptorResources(); 
+    void executeComputeShader(uint32_t groupCountX, uint32_t groupCountY, uint32_t groupCountZ);  
+    cv::Mat readbackOutputBuffer(int width, int height, int channels); 
+    std::vector<u_int32_t> readbackCorners();
     
 
 private:
+
+    uint32_t m_imageWidth = 0; 
+    uint32_t m_imageHeight = 0; 
 
     //Member Variables for General Setup on Apple
     vk::Instance m_instance = nullptr; 
     vk::PhysicalDevice m_physicalDevice = nullptr;
     std::vector<vk::QueueFamilyProperties> m_queueFamilyProperties{};
     vk::Device m_device = nullptr;
+    vk::Queue m_computeQueue = nullptr; 
     float m_queuePriority = 0.0f;
     uint32_t m_computeQueueFamilyIndex = 0;
     vk::CommandPool m_commandPool = nullptr;
     vk::CommandBuffer m_commandBuffer = nullptr;
-    vk::DeviceSize m_deviceSize = 0;
+
 
     //Member for Pipeline 
     vk::ShaderModule m_shaderModule = nullptr;
@@ -39,8 +46,14 @@ private:
     //Member for Buffer 
     vk::Buffer m_InBuffer = nullptr;
     vk::Buffer m_OutBuffer = nullptr;
+    vk::Buffer m_CornerBuffer = nullptr;  
+
     vk::DeviceMemory m_InBufferMemory = nullptr;
     vk::DeviceMemory m_OutBufferMemory = nullptr;
+    vk::DeviceMemory m_CornerBufferMemory = nullptr;
+
+    vk::DeviceSize m_deviceSize = 0;
+    vk::DeviceSize m_cornerBufferSize = 0;
 
 
     //functions for General Setup on Apple
@@ -51,12 +64,16 @@ private:
     void createLogicalDevice(); 
     void createCommandPool();
     void createCommandBuffer();
+    void getComputeQueue();
 
     //Pipeline helper functions 
     std::vector<char> readShaderFile(const std::string& filename);
     void createShaderModule(const std::vector<char>& code);
     void createDescriptorSetLayout();
     void createPipelineLayout();
+
+    //Buffer helper functions
+    void createCornerBuffer(uint32_t width, uint32_t height); 
 
     //Member for Descriptors
     vk::DescriptorPool m_descriptorPool = nullptr;
@@ -66,5 +83,9 @@ private:
     void createDescriptorPool();
     void allocateDescriptorSet();
     void updateDescriptorSet();
+
+    //Execution helper functions
+    void recordCommandBuffer(uint32_t groupCountX, uint32_t groupCountY, uint32_t groupCountZ);
+    void submitAndWait();
 
 };
